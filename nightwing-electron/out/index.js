43553421164c02cb851e243ac9cecfb98fb57c42
Dennis Mitze <dm@knm.io>
2018-11-30 11:57:00 +0100

    ignore build outputs

--- nightwing-electron/out/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var contact_list_1 = require("./contact-list/contact-list");
-exports.ContactList = contact_list_1.ContactList;
-//# sourceMappingURL=index.js.map
